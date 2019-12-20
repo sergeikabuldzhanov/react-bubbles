15a75947b6c1b68c8a32d462967ef151e0525648
sergeikabuldzhanov <thesiegebot@gmail.com>
2019-12-20 14:00:11 +0500

    Setup side-effect to fetch colors in BubblePage

--- client/src/components/BubblePage.js
+++ client/src/components/BubblePage.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import axios from "axios";
+import axios from "../auth";
 
 import Bubbles from "./Bubbles";
 import ColorList from "./ColorList";
@@ -8,7 +8,14 @@ const BubblePage = () => {
   const [colorList, setColorList] = useState([]);
   // fetch your colors data from the server when the component mounts
   // set that data to the colorList state property
-
+  useEffect(()=>{
+    axios().get(`http://localhost:5000/api/colors`)
+      .then(response=>{
+        setColorList(response.data)
+      }).catch(error=>{
+        console.error(error);
+      });
+  },[])
   return (
     <>
       <ColorList colors={colorList} updateColors={setColorList} />
